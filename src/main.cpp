97455ee485bff4361ac8dca899e1743cea0c339b
k88936 <k88936@outlook.com>
2024-12-10 15:38:51 +0800

    lower background sound

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,7 @@ int main(int argc, char* argv[])
     auto player = new QMediaPlayer();
     auto audio = new QAudioOutput();
     player->setAudioOutput(audio);
-    audio->setVolume(0.2);
+    audio->setVolume(0.1);
     do_work(player, "../music/MW3/");
     QObject::connect(player, &QMediaPlayer::mediaStatusChanged, [=](QMediaPlayer::MediaStatus status)
     {
